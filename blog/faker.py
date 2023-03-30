1737d6416b1cb5f29dbb07963cac0e8eb668a14a
Valentyna Feshchenko <fesval777@gmail.com>
2023-03-30 11:31:51 +0200

    commit_1-30.03

--- blog/faker.py
+++ blog/faker.py
@@ -2,13 +2,13 @@ from faker import Faker
 from blog.models import Entry, db
 
 def generate_entries(how_many=10):
-   fake = Faker()
+    fake = Faker()
 
-   for i in range(how_many):
-       post = Entry(
-           title=fake.sentence(),
-           body='\n'.join(fake.paragraphs(15)),
-           is_published=True
-       )
-       db.session.add(post)
-   db.session.commit()
+    for i in range(how_many):
+        post = Entry(
+            title=fake.sentence(),
+            body='\n'.join(fake.paragraphs(15)),
+            is_published=True
+        )
+        db.session.add(post)
+    db.session.commit()
--- blog/routes.py
+++ blog/routes.py
@@ -8,68 +8,67 @@ from blog.forms import EntryForm, LoginForm
 
 @app.route("/")
 def index():
-   all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
+    all_posts = Entry.query.filter_by(is_published=True).order_by(Entry.pub_date.desc())
 
-   return render_template("homepage.html", all_posts=all_posts)
+    return render_template("homepage.html", all_posts=all_posts)
 
 def login_required(view_func):
     @functools.wraps(view_func)
     def check_permissions(*args, **kwargs):
-       if session.get('logged_in'):
-           return view_func(*args, **kwargs)
-       return redirect(url_for('login', next=request.path))
+        if session.get('logged_in'):
+            return view_func(*args, **kwargs)
+        return redirect(url_for('login', next=request.path))
     return check_permissions
 
 @app.route("/login/", methods=['GET', 'POST'])
 def login():
-   form = LoginForm()
-   errors = None
-   next_url = request.args.get('next')
-   if request.method == 'POST':
-       if form.validate_on_submit():
-           session['logged_in'] = True
-           session.permanent = True  # Use cookie to store session.
-           flash('You are now logged in.', 'success')
-           return redirect(next_url or url_for('index'))
-       else:
-           errors = form.errors
-   return render_template("login_form.html", form=form, errors=errors)
+    form = LoginForm()
+    errors = None
+    next_url = request.args.get('next')
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            session['logged_in'] = True
+            session.permanent = True  # Use cookie to store session.
+            flash('You are now logged in.', 'success')
+            return redirect(next_url or url_for('index'))
+        else:
+            errors = form.errors
+    return render_template("login_form.html", form=form, errors=errors)
 
 
 @app.route('/logout/', methods=['GET', 'POST'])
 def logout():
-   if request.method == 'POST':
-       session.clear()
-       flash('You are now logged out.', 'success')
-   return redirect(url_for('index'))
+    if request.method == 'POST':
+        session.clear()
+        flash('You are now logged out.', 'success')
+    return redirect(url_for('index'))
 
 
 @app.route("/posts/<int:entry_id>", methods=["GET", "POST"])
 @login_required
 def create_edit_entry(entry_id):
-   errors = None
+    errors = None
 
-   if entry_id==0: #создаем новую запись
-       
-    form = EntryForm()
-    if request.method == 'POST':
-       if form.validate_on_submit():
-           entry = Entry(
-               title=form.title.data,
-               body=form.body.data,
-               is_published=form.is_published.data,
-           )
-           db.session.add(entry)
-           db.session.commit()
-       else:
-           errors = form.errors
+    if entry_id==0: #создаем новую запись
+        form = EntryForm()
+        if request.method == 'POST':
+            if form.validate_on_submit():
+                entry = Entry(
+                title=form.title.data,
+                body=form.body.data,
+                is_published=form.is_published.data,
+            )
+            db.session.add(entry)
+            db.session.commit()
+        else:
+            errors = form.errors
    
 
     else:  # редактируем старую запись
-       entry = Entry.query.filter_by(id=entry_id).first_or_404()
-       form = EntryForm(obj=entry)
+        entry = Entry.query.filter_by(id=entry_id).first_or_404()
+        form = EntryForm(obj=entry)
    
-       if request.method == 'POST':
+        if request.method == 'POST':
             if form.validate_on_submit():
                 form.populate_obj(entry)
                 db.session.commit()
@@ -83,8 +82,8 @@ def create_edit_entry(entry_id):
 @app.route("/drafts/", methods=['GET'])
 @login_required
 def list_drafts():
-   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
-   return render_template("drafts.html", drafts=drafts)
+    drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+    return render_template("drafts.html", drafts=drafts)
 
 
 @app.route("/posts/<int:entry_id>", methods=["POST"])
